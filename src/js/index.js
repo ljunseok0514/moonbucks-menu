f85071dfd7dd8bcf53599861ccea96f63e17c402
ljunseok0514 <73566234+ljunseok0514@users.noreply.github.com>
2022-11-15 08:29:48 +0900

    221115

---
+++ src/js/index.js
@@ -0,0 +1,63 @@
+// // // step1 요구사항 구현을 위한 전략
+// TODO 메뉴 추가
+// - [ ] 메뉴의 이름을 입력받고 엔터키 입력으로 추가한다.
+// - [ ] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [ ] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다.
+// - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+const $ = (selector) => document.querySelector(selector);
+
+function App() {
+  // form태그가 자동으로 전송되는걸 막아준다.
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  //메뉴의 이름을 입력받는건
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const espressMunuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = (espressMunuName) => {
+        return `
+        <li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressMunuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
+      };
+      // <!-- beforebegin -->
+      // <ul>
+      // <!-- afterbegin -->
+      // <li></li>.insertAdjacentHTML('afterend', '<div id="two">two</div>');
+      // <!-- beforeend -->
+      // </ul>
+      // <!-- afterend -->
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate(espressMunuName)
+      );
+    }
+  });
+}
+
+App();
+
+//TODO 메뉴 수정
+// - [ ] 메뉴의 수정 버튼 클릭 이벤트를 받고, `prompt`창이 뜬다.
+// - [ ] `prompt`에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+//TODO 메뉴 삭제
+// - [ ] 메뉴 삭제 버튼클릭 이벤트를 받고, 메뉴 삭제 `confirm` 창이 뜬다.
+// - [ ] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
